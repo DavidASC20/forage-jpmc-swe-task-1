1589082a86ee452c67a2672aac473af8394cd9c7
David Chong <djc@mbp.local>
2023-07-14 14:44:11 -0400

    change how price is calculated in the event that bid or ask is 0, add comments to code for better understanding in the future, and tested edge case of bid or ask being 0.

--- client3.py
+++ client3.py
@@ -35,29 +35,44 @@ def getDataPoint(quote):
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = (bid_price + ask_price) / 2
+    #account for case where ask or bid is 0/not available, assuming that both arent zero
+    if bid_price == 0:
+        price = ask_price
+    elif ask_price == 0:
+        price = bid_price
+    else:
+        price = (bid_price + ask_price) / 2
     return stock, bid_price, ask_price, price
 
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
+
+    #if the amount of asks are 0, a ratio cannot be calculated
+    #return price a, if there are only bid price, theres no ratio
     if(price_b == 0):
-        return 
+        return price_a
+    #otherwise, return the ratio of the bid price and ask price
     return price_a/price_b
 
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
+    # For each query, 2 quotes
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
 
         """ ----------- Update to get the ratio --------------- """
+        #create a dictionary to store the ticker of stock as the key and the price of the stock (average ofbid price + ask price) as value
         prices = {}
+        #for each quote
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            #get the data from the tuple, store the stock and its price into the dictionary
             prices[stock] = price
+            #print the data from the tuple
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-
+        #this works because each quote has name ABC and DEF, updating the information in the dictionary
         print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
--- client_test.py
+++ client_test.py
@@ -8,6 +8,12 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    # for each quote, assert that the tuple returned from the function is what it is supposed to be
+    for quote in quotes:
+      ticker = quote['stock']
+      bid_price = quote['top_bid']['price'] 
+      ask_price = quote['top_ask']['price']
+      self.assertEqual(getDataPoint(quote), (ticker, bid_price, ask_price, (bid_price + ask_price) / 2 ))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,9 +21,35 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    # for each quote, assert that the tuple returned from the function is what it is supposed to be
+    for quote in quotes:
+      ticker = quote['stock']
+      bid_price = quote['top_bid']['price'] 
+      ask_price = quote['top_ask']['price']
+      self.assertEqual(getDataPoint(quote), (ticker, bid_price, ask_price, (bid_price + ask_price) / 2 ))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getDataPoint_calculatePriceAskisZero(self):
+    quotes = [
+      {'top_ask': {'price': 0, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    for quote in quotes:
+      ticker = quote['stock']
+      bid_price = quote['top_bid']['price'] 
+      self.assertEqual(getDataPoint(quote), (ticker, bid_price, 0, bid_price))
+
+  
+  def test_getDataPoint_calculatePriceBidisZero(self):
+    quotes = [
+      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    for quote in quotes:
+      ticker = quote['stock']
+      ask_price = quote['top_ask']['price'] 
+      self.assertEqual(getDataPoint(quote), (ticker, 0, ask_price, ask_price))
+
 
 
 
